654127ce87e88590bea9005a6c0542b6ebaff7d5
Makvi-droid <pasbenauro05@gmail.com>
2024-12-26 15:00:19 +0800

    added files

---
+++ objects/practices/practice-one.js
@@ -0,0 +1,7 @@
+const person = {
+    name: "Alice",
+    age: 25,
+    isStudent: true,
+    hobbies:
+    ["reading", "travelling", "coding"]
+};
--- objects/script.js
+++ objects/script.js
@@ -2,5 +2,19 @@ const game = {
     name: "Bloodborne",
     genre: "soulsborne",
     releaseDate: 2015,
-    author: "Hidetaka Miyazaki"
+    author: "Hidetaka Miyazaki",
+    award: function(){
+        console.log(`${this.name} won the game awards!!`);
+    },
+
+    created: function(){
+        return "It was created by " + this.author;
+    }
 };
+
+game.award();
+game.genre;
+game.created();
+
+paragraph = document.getElementById('author');
+paragraph.textContent = game.created();
